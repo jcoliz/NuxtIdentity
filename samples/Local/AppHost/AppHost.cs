9876aa752ec6ec029a5f8826420e090af97245ef
James Coliz <James.Coliz@microsoft.com>
2025-11-15 22:47:46 -0800

    build(s/local): Wire up frontend to aspire

--- samples/Local/AppHost/AppHost.cs
+++ samples/Local/AppHost/AppHost.cs
@@ -3,4 +3,11 @@ var builder = DistributedApplication.CreateBuilder(args);
 var backend = builder.AddProject<Projects.NuxtIdentity_Samples_Local_Backend>("backend")
     .WithHttpHealthCheck("/health");
 
+builder.AddNpmApp("frontend", "../Frontend")
+    .WithReference(backend)
+    .WaitFor(backend)
+    .WithHttpEndpoint(port: 3000, env: "PORT")
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
+
 builder.Build().Run();
