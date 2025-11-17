8f20aef94a93b1e024f30b8be98568c2ce5db9c8
James Coliz <James.Coliz@microsoft.com>
2025-11-16 18:26:47 -0800

    refactor(s/local): Remove needless boilerplate

--- samples/Local/Backend/Program.cs
+++ samples/Local/Backend/Program.cs
@@ -96,33 +96,4 @@ app.UseAuthorization();
 app.MapDefaultEndpoints();
 app.MapControllers();
 
-#if false
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithSummary("Get weather forecast")
-.WithDescription("Returns a 5-day weather forecast")
-.WithTags("Weather");
-#endif
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
